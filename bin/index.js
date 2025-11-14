034e4e3eb92290813d10e517d7943f8b4554fd4c
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-11-14 23:20:52 +0400

    iCLI v 1.0.0 express app generator

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import { generateExpress } from "../generators/express.js";
+
+program.name("icli").description("OWN Cli").version("1.0.0");
+
+program
+  .command("express")
+  .description("Generate Express project")
+  .argument("<myApp>", "Name of the project")
+  .action((myApp) => {
+    generateExpress(myApp);
+  });
+
+program.parse();
---
+++ generators/express.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import { execSync } from "child_process";
+import { mkdirSync, cpSync } from "fs";
+import path from "path";
+import { cwd } from "process";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function generateExpress(myApp) {
+  const projectPath = path.join(cwd(), myApp);
+  mkdirSync(projectPath, { recursive: true });
+
+  const templatePath = path.join(__dirname, "../templates/express");
+
+  cpSync(templatePath, projectPath, {
+    recursive: true,
+    force: true,
+    errorOnExist: false,
+  });
+
+  console.log(`${myApp}'s Express template created.`);
+
+  console.log("Packages installing...");
+
+  try {
+    execSync("npm install express cors dotenv", {
+      cwd: projectPath,
+      stdio: "inherit",
+    });
+
+    execSync("npm install -D nodemon", {
+      cwd: projectPath,
+      stdio: "inherit",
+    });
+
+    console.log("Project ready!");
+    console.log(`\If start, write:\n  cd ${myApp}\n  npm run dev`);
+  } catch (error) {
+    console.error("Error when packages installing:", error.message);
+  }
+}
