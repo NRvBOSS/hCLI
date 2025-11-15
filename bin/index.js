e6a83b77084510b71fd9c1c55fe8634a5d099eee
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-11-15 23:03:14 +0400

    v 1.0.1 / add banner text, download packages

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,36 @@
 #!/usr/bin/env node
 import { program } from "commander";
 import { generateExpress } from "../generators/express.js";
-
-program.name("icli").description("OWN Cli").version("1.0.0");
+import figlet from "figlet";
+import gradient from "gradient-string";
 
 program
-  .command("express")
-  .description("Generate Express project")
-  .argument("<myApp>", "Name of the project")
-  .action((myApp) => {
-    generateExpress(myApp);
-  });
+  .name("icli")
+  .version("1.0.1")
+  .addHelpCommand(false);
 
-program.parse();
+function bigCliName() {
+  return new Promise((resolve, reject) => {
+    console.clear();
+    const msg = "iCLI";
+
+    figlet(msg, (err, data) => {
+      if (err) reject(err);
+      console.log(gradient.mind.multiline(data));
+      resolve();
+    });
+  });
+}
+
+(async () => {
+  await bigCliName();
+  program
+    .command("express")
+    .description("Generate Express project")
+    .argument("<myApp>", "Name of the project")
+    .action((myApp) => {
+      generateExpress(myApp);
+    });
+
+  program.parse();
+})();
