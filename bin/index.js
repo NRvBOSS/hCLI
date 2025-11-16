8c35f54b58d5ad4c6bcd262e97f948f12d3eed57
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-11-16 21:44:01 +0400

    error handling

--- bin/index.js
+++ bin/index.js
@@ -11,42 +11,52 @@ program.name("icli").version("1.1.0").addHelpCommand(false);
 callExpress();
 
 async function run() {
-  // DIRECT MODE
-  if (process.argv.length > 2) {
+  try {
+    // DIRECT MODE
+    if (process.argv.length > 2) {
+      await bigCliName();
+      program.parse();
+      return;
+    }
+
+    // INTERACTIVE MODE
     await bigCliName();
-    program.parse();
-    return;
-  }
 
-  // INTERACTIVE MODE
-  await bigCliName();
-
-  const { generator } = await inquirer.prompt([
-    {
-      type: "list",
-      message: "Pick the generator for using:",
-      name: "generator",
-      choices: ["Express.JS generator", "Vue.JS generator"],
-    },
-  ]);
-
-  if (generator === "Express.JS generator") {
-    const { projectName } = await inquirer.prompt([
+    const { generator } = await inquirer.prompt([
       {
-        type: "input",
-        name: "projectName",
-        message: "Write your project name:",
-        validate: (input) =>
-          input.trim() !== "" ? true : "Project name cannot be empty",
+        type: "list",
+        message: "Pick the generator for using:",
+        name: "generator",
+        choices: ["Express.JS generator", "Vue.JS generator"],
       },
     ]);
 
-    generateExpress(projectName);
-    return;
-  }
+    if (generator === "Express.JS generator") {
+      const { projectName } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "projectName",
+          message: "Write your project name:",
+          validate: (input) =>
+            input.trim() !== "" ? true : "Project name cannot be empty",
+        },
+      ]);
 
-  if (generator === "Vue.JS generator") {
-    console.log(chalk.green("Coming soon..."));
+      generateExpress(projectName);
+      return;
+    }
+
+    if (generator === "Vue.JS generator") {
+      console.log(chalk.green("Coming soon..."));
+    }
+  } catch (err) {
+    // CTRL+C HANDLING
+    if (err.name === "ExitPromptError") {
+      console.log(chalk.red("\nProcess canceled by user (Ctrl+C)."));
+      process.exit(0);
+    }
+
+    console.log("Unexpected error:", err);
   }
 }
 
